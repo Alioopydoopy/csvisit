0ca0d96aafb9752efd998cb581eac41aa1a12b60
Alioopydoopy <SylviaJensen2006@gmail.com>
2025-04-11 17:14:53 +0000

    scanner added

--- yay.java
+++ yay.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class yay{
-    public static void main (String[] args){
+    public void greet(){
         System.out.println("Helllo world! you stinkgood!!!!");
-
     }
+    public static void main (String[] args){
+yay guy= new yay();
+yay.greet();
+Scanner scanner= new Scanner(System.in);
+System.out.println("enter your name");
+String name= scanner.nextLine();
+System.out.println("HI " + name);
+    }
+
+
 }
